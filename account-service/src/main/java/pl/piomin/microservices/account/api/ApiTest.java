6beee398dec3b9251ae882111a69cc3758787251
vivekragi <44323873+vivekragi@users.noreply.github.com>
2019-09-03 10:35:07 +0530

    Create ApiTest.java

---
+++ account-service/src/main/java/pl/piomin/microservices/account/api/ApiTest.java
@@ -0,0 +1,14 @@
+package pl.piomin.microservices.account.api;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class APiTest{
+
+@Test
+    public void testGetProduct_id() {
+        assertEquals(new Long(1),new Long(1));
+    }
+
+}
